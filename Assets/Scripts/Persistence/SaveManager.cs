7d7e1db4dbbf3058824aeb3f232de1f0e58d06d6
Richard James <richard@richardpjames.com>
2024-10-12 09:36:59 +0100

    Minor tweaks

--- Assets/Scripts/Persistence/SaveManager.cs
+++ Assets/Scripts/Persistence/SaveManager.cs
@@ -7,22 +7,24 @@ public class SaveManager : MonoBehaviour
     {
         // Get reference to the world
         World world = GameObject.FindAnyObjectByType<World>();
+        string savePath = $"{Application.persistentDataPath}/{world.Name}.json";
         // Create a save and then populate it from the appropriate objects
         GameSave save = new GameSave();
         save.WorldSave = world.Serialize();
         // Finally convert to a save file
         string jsonSave = JsonConvert.SerializeObject(save);
-        System.IO.File.WriteAllText($"{Application.persistentDataPath}/{world.Name}.json", jsonSave);
+        System.IO.File.WriteAllText(savePath, jsonSave);
     }
 
     public void Load()
     {
         // Get reference to the world
         World world = GameObject.FindAnyObjectByType<World>();
+        string loadPath = $"{Application.persistentDataPath}/{world.Name}.json";
         // Return if the file to load doesn't exist
-        if (!System.IO.File.Exists($"{Application.persistentDataPath}/{world.Name}.json")) return;
+        if (!System.IO.File.Exists(loadPath)) return;
         // Read the data from the file
-        string jsonData = System.IO.File.ReadAllText($"{Application.persistentDataPath}/{world.Name}.json");
+        string jsonData = System.IO.File.ReadAllText(loadPath);
         // Convert back into a game save
         GameSave save = JsonConvert.DeserializeObject<GameSave>(jsonData);
         // Then get the world to load the data
