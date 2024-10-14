ad3f5fd6e7abdf5af055c0db7d25990b8e270522
Richard James <richard@richardpjames.com>
2024-10-14 11:50:12 +0100

    Windows now come to top on focus

--- Assets/Scripts/UI/TitleBar.cs
+++ Assets/Scripts/UI/TitleBar.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TitleBar : MonoBehaviour, IDragHandler
+public class TitleBar : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
     [SerializeField] private Canvas _canvas;
     [SerializeField] private RectTransform _panel;
+
+    // This brings the window to the top on click
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _panel.SetAsLastSibling();
+    }
+
+    // This provides drag behaviour for the window
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
         _panel.anchoredPosition += eventData.delta / _canvas.scaleFactor;
